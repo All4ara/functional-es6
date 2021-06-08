9d59478e0422da42fe1a85ac7209e6cf65ac93c2
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-06-08 14:49:16 -0400

    eslint setup

--- hello-world.js
+++ hello-world.js
@@ -2,3 +2,11 @@ const sayHello = name => console.log(`hello ${name}`)
 
 sayHello("ariel")
 
+
+//Learning eslint to catch any mutation to const variables
+const person = { 
+    name: 'John Doe',
+    age: 27,
+};
+
+person.name = "bob";
